50e5d8bafa8d96d94b5cd670346607fbfbddbb97
Aleksandar <aleksandarprojekat@gmail.com>
2024-06-30 14:15:15 +0200

    testbench pocetak bez axi

--- RTL/ip_tb.vhd
+++ RTL/ip_tb.vhd
@@ -27,6 +27,40 @@ file pixels1D : text open read_mode is
     constant IMG_WIDTH : integer := 128;
     constant IMG_HEIGHT : integer := 128;
 
+------------------Ports for BRAM Initialization-----------------
+    
+    signal tb_a_en_i : std_logic;
+    signal tb_a_addr_i : std_logic_vector(PIXEL_SIZE-1 downto 0);
+    signal tb_a_data_i : std_logic_vector(7 downto 0);
+    signal tb_a_we_i : std_logic;
+    
+    signal tb_b_en_i : std_logic;
+    signal tb_b_addr_i : std_logic_vector(PIXEL_SIZE-1 downto 0);
+    signal tb_b_data_i : std_logic_vector(7 downto 0);
+    signal tb_b_we_i : std_logic; 
+    
+    signal tb_c_en_i : std_logic;
+    signal tb_c_addr_i : std_logic_vector(5 downto 0);
+    signal tb_c_data_o : std_logic_vector(8*FIXED_SIZE + 4*WIDTH + 2*SUM_WIDTH - 1 downto 0);
+    signal tb_c_we_i : std_logic;
+    
+     ------------------------- Ports to IP ---------------------
+    
+    signal ip_a_en : std_logic;
+    signal ip_a_we : std_logic;
+    signal ip_a_addr : std_logic_vector(PIXEL_SIZE-1 downto 0);
+    signal ip_a_data: std_logic_vector(7 downto 0);
+    
+    signal ip_b_en : std_logic;
+    signal ip_b_we : std_logic;
+    signal ip_b_addr : std_logic_vector(PIXEL_SIZE-1 downto 0);
+    signal ip_b_data: std_logic_vector(7 downto 0);
+    
+    signal ip_c_en : std_logic;
+    signal ip_c_we : std_logic;
+    signal ip_c_addr : std_logic_vector(5 downto 0);
+    signal ip_c_data: std_logic_vector(8*FIXED_SIZE + 4*WIDTH + 2*SUM_WIDTH - 1 downto 0);
+    
     -- Komponente za testiranje
     component ip
         generic (
@@ -91,8 +125,8 @@ file pixels1D : text open read_mode is
     end component;
 
     -- Signali za testiranje
-    signal clk : std_logic := '0';
-    signal reset : std_logic := '0';
+    signal clk_s : std_logic := '0';
+    signal reset_s : std_logic := '0';
     signal iradius : unsigned(WIDTH - 1 downto 0) := (others => '0');
     signal fracr : std_logic_vector(FIXED_SIZE - 1 downto 0) := (others => '0');
     signal fracc : std_logic_vector(FIXED_SIZE - 1 downto 0) := (others => '0');
@@ -139,7 +173,7 @@ begin
             SIZE_WIDTH => 6
         )
         port map (
-            clk_a => clk,
+            clk_a => clk_s,
             en_a => rom_en_a,
             addr_a => rom_addr,
             data_a_o => rom_data
@@ -157,8 +191,8 @@ begin
             IMG_HEIGHT => IMG_HEIGHT
         )
         port map (
-            clk => clk,
-            reset => reset,
+            clk => clk_s,
+            reset => reset_s,
             iradius => iradius,
             fracr => fracr,
             fracc => fracc,
@@ -188,19 +222,155 @@ begin
             start_i => start_i,
             ready_o => ready_o
         );
- -- Instanciranje BRAM-a za ulazne podatke
+ 
+        
+        
+    -- Proces za generisanje taktnog signala
+   clk_gen: process is
+    begin
+        clk_s <= '0', '1' after 10 ns;
+        wait for 20 ns;
+    end process;
+
+ 
+
+    stimulus_generator: process
+    variable tv_slika : line;
+    begin
+    report "Start !";
+    
+    -- reset signal
+    reset_s <= '1';
+    wait for 5*20 ns; -- wait for 5 clock cycles
+    reset_s <= '0';
+    wait for 5*20 ns; -- wait for 5 clock cycles
+        
+    -- Initialize the core --
+    report "Loading the picture dimensions into the core!" ;   
+        -- Unos ulaznih signala
+        
+   -- Load the picture into the memory
+    report "Loading picture into the memory!" ;
+    for i in 0 to (IMG_WIDTH*IMG_HEIGHT)-1 loop 
+        wait until falling_edge(clk_s);
+        readline(pixels1D, tv_slika);
+        tb_a_en_i <= '1';
+        tb_a_addr_i <= conv_std_logic_vector(0, PIXEL_SIZE-1); 
+        tb_a_data_i <= to_std_logic_vector(string(tv_slika));
+        tb_a_we_i <= '1';
+        
+        tb_b_en_i <= '1';
+        tb_b_addr_i <= conv_std_logic_vector(0, PIXEL_SIZE-1); 
+        tb_b_data_i <= to_std_logic_vector(string(tv_slika));
+        tb_b_we_i <= '1';
+
+        for j in 1 to 3 loop
+            wait until falling_edge(clk_s);
+        end loop;
+        tb_a_en_i <= '0';
+        tb_a_we_i <= '0'; 
+        tb_b_en_i <= '0';
+        tb_b_we_i <= '0';
+        
+    end loop;
+        tb_a_en_i <= '0';
+        tb_a_we_i <= '0'; 
+        tb_b_en_i <= '0';
+        tb_b_we_i <= '0';    
+        
+        -- Start the IP core processing
+    report "Starting processing!";
+    ip_a_en <= '1';
+    ip_b_en <= '1';
+    ip_c_en <= '1';
+
+    -- Simulate processing time
+    wait for 1000 ns;
+
+    ip_a_en <= '0';
+    ip_b_en <= '0';
+    ip_c_en <= '0';
+    
+    -- Read the output
+    report "Reading the results from output memory!";
+    for k in 0 to (IMG_WIDTH*IMG_HEIGHT) loop
+        wait until falling_edge(clk_s);
+        tb_c_en_i <= '1';
+        tb_c_we_i <= '0';
+        tb_c_addr_i <= conv_std_logic_vector(0, 5);
+    end loop;
+
+    tb_c_en_i <= '0';
+    report "Finished!";
+    report "RESULTS MATCH!";
+    wait;
+
+end process;
+
+write_to_output_file : process(clk_s)
+    variable data_output_line : line;
+    variable data_output_string : string(1 to 24) := (others => '0'); 
+begin
+    if falling_edge(clk_s) then
+        if tb_c_en_i = '1' then
+            data_output_string := (others => '0');
+            for i in 0 to 23 loop
+                if tb_c_data_o(i) = '1' then
+                    data_output_string(24 - i) := '1';  
+                else
+                    data_output_string(24 - i) := '0';  
+                end if;
+            end loop;          
+            write(data_output_line, data_output_string);
+            writeline(izlaz, data_output_line);
+        end if;
+    end if;
+end process;
+
+checker : process(clk_s)
+    variable tv_izlazi : line;  
+    variable tmp: std_logic_vector(3*WIDTH-1 downto 0); 
+begin              
+    if falling_edge (clk_s) then
+        if tb_c_en_i = '1' then
+            readline(index1Dbin, tv_izlazi);
+            tmp := to_std_logic_vector(string(tv_izlazi));
+            if (tmp /= tb_c_data_o) then
+                report "RESULT MISMATCH" severity failure;
+            end if;
+        end if;
+    end if;
+end process;
+        
+    iradius <= to_unsigned(17, WIDTH); -- 17 je decimalna vrednost za iradius
+    fracr <= "000000000000000000000000000000001010000011000010"; --  0.15699263069083616 u binarnom formatu
+                                                                
+
+    fracc <= "000000000000000000000000000000011100110110110110"; --  0.45089018167010253 u binarnom formatu
+    spacing <= "000000000000000000000000001001101011111000001110"; -- 9.685600969817237 u binarnom formatu
+                                                                  
+
+    iy <= to_unsigned(38, WIDTH); -- 38 je decimalna vrednost za iy
+    ix <= to_unsigned(64, WIDTH); -- 64 je decimalna vrednost za ix
+    step <= to_unsigned(2, WIDTH); -- 2 je decimalna vrednost za step
+    i_cose <= "000000000000000000000000000000011011011001010001"; --  0.428043365478515625 u binarnom formatu
+    i_sine <= "000000000000000000000000000000111001110101110010"; -- 0.90375518798828125 u binarnom formatu
+    scale <= "000000000000000000000000000011001110101000000100"; -- 3.2285336566057454 u binarnom formatu
+    rom_en_a <= '1';
+
+-- Instanciranje BRAM-a za ulazne podatke
     bram_in: entity work.bram
         generic map (
             WIDTH => 8,  -- širina podataka
             BRAM_SIZE => IMG_WIDTH*IMG_HEIGHT  -- dubina memorije
         )
         port map (
-            clka => clk,
-            clkb => clk,
+            clka => clk_s,
+            clkb => clk_s,
             ena => bram_en1_o,
             enb => bram_en2_o,
-            reseta => reset,
-            resetb => reset,
+            reseta => reset_s,
+            resetb => reset_s,
             wea => open,
             web => open,
             addra => bram_addr1_o,
@@ -218,12 +388,12 @@ begin
             BRAM_SIZE => IMG_WIDTH*IMG_HEIGHT  -- dubina memorije
         )
         port map (
-            clka => clk,
-            clkb => clk,
+            clka => clk_s,
+            clkb => clk_s,
             ena => c1_data_o,
             enb => c2_data_o,
-            reseta => reset,
-            resetb => reset,
+            reseta => reset_s,
+            resetb => reset_s,
             wea => bram_we1_o,
             web => bram_we1_o,
             addra => addr_do1_o,
@@ -233,62 +403,5 @@ begin
             doa => data1_o_next,
             dob => data2_o_next
         );
-        
-        
-    -- Proces za generisanje taktnog signala
-    clk_process :process
-    begin
-        clk <= '0';
-        wait for clk_period/2;
-        clk <= '1';
-        wait for clk_period/2;
-    end process;
-
- 
-
-    -- Stimulus process
-    stim_proc: process
-    begin    
-        -- Resetovanje sistema
-        reset <= '1';
-        wait for clk_period*10; -- Duže trajanje reset signala
-        reset <= '0';
-        wait for clk_period*2;
-        
-        -- Unos ulaznih signala
-    iradius <= to_unsigned(17, WIDTH); -- 17 je decimalna vrednost za iradius
-    fracr <= "000000000000000000000000000000001010000011000010"; --  0.15699263069083616 u binarnom formatu
-                                                                
-
-    fracc <= "000000000000000000000000000000011100110110110110"; --  0.45089018167010253 u binarnom formatu
-    spacing <= "000000000000000000000000001001101011111000001110"; -- 9.685600969817237 u binarnom formatu
-                                                                  
-
-    iy <= to_unsigned(38, WIDTH); -- 38 je decimalna vrednost za iy
-    ix <= to_unsigned(64, WIDTH); -- 64 je decimalna vrednost za ix
-    step <= to_unsigned(2, WIDTH); -- 2 je decimalna vrednost za step
-    i_cose <= "000000000000000000000000000000011011011001010001"; --  0.428043365478515625 u binarnom formatu
-    i_sine <= "000000000000000000000000000000111001110101110010"; -- 0.90375518798828125 u binarnom formatu
-    scale <= "000000000000000000000000000011001110101000000100"; -- 3.2285336566057454 u binarnom formatu
-    rom_en_a <= '1';
-
-        
-        
-        -- Pokretanje
-        start_i <= '1';
-        wait for clk_period*2;
-        start_i <= '0';
-        
-        -- ?ekanje da se obrada završi
-        wait until ready_o = '1';
-        
-        -- Prolazak kroz razli?ite stanja
-        for i in 0 to 1000 loop
-            wait for clk_period;
-        end loop;
-        
-        -- Završetak simulacije
-        wait;
-    end process;
 
 end Behavioral;
